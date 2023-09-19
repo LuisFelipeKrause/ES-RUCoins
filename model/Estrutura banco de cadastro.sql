8bfb8b896936cb9a65d1eb66ac0ce988a7af6ab6
LuFi-1227 <luizsalvesp@protonmail.com>
2023-09-19 13:25:28 -0300

    atualização

---
+++ model/Estrutura banco de cadastro.sql
@@ -0,0 +1,51 @@
+CREATE SCHEMA `loginRU` ;
+
+create table legendas_estado(
+	legendas_estado_id integer not null auto_increment primary key,
+    estado varchar(45)
+);
+
+insert into legendas_estado(estado)
+values
+("inativo"),
+("ativo");
+
+create table legendas_permissao(
+	legendas_permissao_id integer not null auto_increment primary key,
+    permissao varchar(45)
+);
+
+insert into legendas_permissao(permissao)
+values
+("Gerente de TI"),
+("Administrador"),
+("Atendente"),
+("Cobrador"),
+("Usuario");
+
+create table usuarios(
+usuario_id integer not null auto_increment primary key,
+nome varchar(45),
+sobrenome varchar(45),
+cpf varchar(45) not null,
+email varchar(45) not null,
+senha varchar(40) not null,
+data_de_nascimento date,
+permissao integer,
+estado_conta integer,
+data_criacao date,
+data_atualizacao date
+);
+
+alter table usuarios add foreign key (permissao) references legendas_permissao(legendas_permissao_id);
+alter table usuarios add foreign key (estado_conta) references legendas_estado(legendas_estado_id);
+
+
+create table saldo(
+	saldo_id integer not null auto_increment primary key,
+    usuario_id integer,
+    saldo double,
+    data_criacao date,
+	data_atualizacao date
+);
+alter table saldo add foreign key (usuario_id) references usuarios(usuario_id);
